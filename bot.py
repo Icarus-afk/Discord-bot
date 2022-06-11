36d4d1412928e547af0eca3e5c7ce26a83e3e298
Ehasan Ahmed <84353981+Icarus-afk@users.noreply.github.com>
2022-06-11 19:22:16 +0600

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import os
+
+#import all of the cogs
+from help_cog import help_cog
+from music_cog import music_cog
+
+bot = commands.Bot(command_prefix='<')
+
+#remove the default help command so that we can write out own
+bot.remove_command('help')
+
+#register the class with the bot
+bot.add_cog(help_cog(bot))
+bot.add_cog(music_cog(bot))
+
+#start the bot with our token
+
+bot.run("OTg0NDQ5OTU2MDM3NDA2NzUw.GiuFbH.tKGs-oHIOCqCIuW0OqmoEjhi_HfSEFgfb3MPrc")
