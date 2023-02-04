aa3334c15ba5984e56493af374cb350a9aadff12
Icarus-afk <ahmed.ehsan1258@gmail.com>
2023-02-04 18:07:26 +0600

    wikibot

--- bot.py
+++ bot.py
@@ -1,20 +1,44 @@
 import discord
-from discord.ext import commands
-import os
+import wikipediaapi
+import random
+import asyncio
+import requests
 
-#import all of the cogs
-from help_cog import help_cog
-from music_cog import music_cog
+# Initialize the Discord client
+client = discord.Client(intents=discord.Intents.default())
 
-bot = commands.Bot(command_prefix='<')
+# Initialize the Wikipedia API client
+wiki = wikipediaapi.Wikipedia(
+    language='en',
+    extract_format=wikipediaapi.ExtractFormat.WIKI
+)
 
-#remove the default help command so that we can write out own
-bot.remove_command('help')
+def get_random_word():
+    # Get a random article from the Wikipedia API
+    response = requests.get("https://en.wikipedia.org/api/rest_v1/page/random/summary")
+    data = response.json()
+    title = data["title"]
+    return title
 
-#register the class with the bot
-bot.add_cog(help_cog(bot))
-bot.add_cog(music_cog(bot))
 
-#start the bot with our token
+# Function to get the summary of a random Wikipedia article
+def get_summary():
+    word = get_random_word()
+    article = requests.get("https://en.wikipedia.org/api/rest_v1/page/"+word+"/summary")
+    print(article)
+    data_article = article.json()
+    final_response = data_article["extract"]
+    link = "https://en.wikipedia.org/wiki/" + word
+    return (final_response,link)
 
-bot.run("token")
+
+# Event that fires when the bot is ready to start
+@client.event
+async def on_ready():
+    channel = client.get_channel(client.guilds[0].text_channels[0].id)
+    while True:
+        summary, link = get_summary()
+        await channel.send(summary)
+        await asyncio.sleep(15)
+
+client.run('MTA0MDMzMTMwMjMyMDIxODEyMw.G14ZZ2.93dBjTmOxR8jgGEZ_jlGly-jc5WVmVgT4dpnGQ')
