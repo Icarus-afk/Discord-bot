04071d005961c60016eaf926775140f0efacbf3e
Icarus-afk <ahmed.ehsan1258@gmail.com>
2023-02-04 19:24:03 +0600

    wikibot

--- bot.py
+++ bot.py
@@ -20,25 +20,28 @@ def get_random_word():
     title = data["title"]
     return title
 
-
 # Function to get the summary of a random Wikipedia article
 def get_summary():
     word = get_random_word()
-    article = requests.get("https://en.wikipedia.org/api/rest_v1/page/"+word+"/summary")
-    print(article)
-    data_article = article.json()
-    final_response = data_article["extract"]
-    link = "https://en.wikipedia.org/wiki/" + word
-    return (final_response,link)
-
+    page = wiki.page(word)
+    summary = page.summary
+    word_list = word.split(" ")
+    link = "https://en.wikipedia.org/wiki/"
+    for word in word_list:
+        link += word + "_"
+    return (summary,link)
 
 # Event that fires when the bot is ready to start
 @client.event
 async def on_ready():
-    channel = client.get_channel(client.guilds[0].text_channels[0].id)
-    while True:
-        summary, link = get_summary()
-        await channel.send(summary)
-        await asyncio.sleep(15)
+    while not client.is_closed():
+        for guild in client.guilds:
+            for channel in guild.text_channels:
+                if channel.name == "spam" and channel.permissions_for(guild.me).send_messages:   
+                    summary, link = get_summary()
+                    await channel.send(summary+"\nread full article: "+link)
+                    await asyncio.sleep(300)
+    
+    
 
-client.run('MTA0MDMzMTMwMjMyMDIxODEyMw.G14ZZ2.93dBjTmOxR8jgGEZ_jlGly-jc5WVmVgT4dpnGQ')
+client.run('token')
